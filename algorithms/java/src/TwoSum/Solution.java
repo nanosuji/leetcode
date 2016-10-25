f80b05a94fa7f2c66ab56a0000ee8c3021448dd1
nanosuji <gogopocoso@gmail.com>
2016-10-24 23:49:00 -0700

    Initial commit.

---
+++ algorithms/java/src/TwoSum/Solution.java
@@ -0,0 +1,8 @@
+package TwoSum;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = {1, 2, 3};
+        return ans;
+    }
+}
---
+++ algorithms/java/src/TwoSum/TwoSumTest.java
@@ -0,0 +1,11 @@
+package TwoSum;
+
+public class TwoSumTest {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] x = {2, 3, 4};
+        int[] sol = s.twoSum(x, 3);
+        System.out.println(sol);
+    }
+}
