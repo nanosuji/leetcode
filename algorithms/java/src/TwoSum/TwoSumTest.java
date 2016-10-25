4fcaa16b5a4fba3953527b557a9fe8e118acf8b1
nanosuji <gogopocoso@gmail.com>
2016-10-25 09:35:26 -0700

    Add sol to TwoSum.

--- algorithms/java/src/TwoSum/TwoSumTest.java
+++ algorithms/java/src/TwoSum/TwoSumTest.java
@@ -1,11 +1,13 @@
 package TwoSum;
 
+import java.util.Arrays;
+
 public class TwoSumTest {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] x = {2, 3, 4};
-        int[] sol = s.twoSum(x, 3);
-        System.out.println(sol);
+        int[] x = {3, 2, 4};
+        int[] sol = s.twoSum(x, 6);
+        System.out.println(Arrays.toString(sol));
     }
 }
